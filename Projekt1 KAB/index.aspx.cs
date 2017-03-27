efd707f3d2e5efb26843cc3ba9cb4406f42ab740
elinnea <elinnea.larsson@gmail.com>
2017-03-27 16:24:28 +0200

    testar att lista artiklar på Index

--- Projekt1 KAB/index.aspx.cs
+++ Projekt1 KAB/index.aspx.cs
@@ -6,18 +6,31 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibraryKAB;
 
+
 namespace Projekt1_KAB
 {
     public partial class index : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            List<Article> articles = new List<Article>();
+            SQL mySQL = new SQL();
 
-            foreach (var item in articles)
+            List<Article> articles = SQL.ReadArticles();
+
+            foreach (Article item in articles)
             {
-               
+                LiteralArticleName.Text +=$"<ul><li>{item.ArticleName}</li>";
+                LiteralArticleDescription.Text += $"<li>{item.ArticleDescription}</li>";
+                LiteralPrice.Text += $"<li>{item.ArticlePrice}</li></ul>";
+
+                if (item.IsInStock == true)
+                {
+                    CheckBoxInStock.Checked = true;
+                }
             }
+
         }
+
     }
+
 }
