8b6dc18280607e56a57fe9b7df67a18a580219c5
elinnea <elinnea.larsson@gmail.com>
2017-03-27 17:14:59 +0200

    Listar artiklar på index

--- Projekt1 KAB/index.aspx
+++ Projekt1 KAB/index.aspx
@@ -2,15 +2,11 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    
-    <div id ="articles">        
-        <asp:PlaceHolder ID="PlaceHolder1" runat="server">
-            <asp:Literal ID="LiteralArticleName" runat="server"></asp:Literal>
-            <asp:Image ID="Image1" runat="server" />
-            <asp:Literal ID="LiteralArticleDescription" runat="server"></asp:Literal>
-            <asp:Literal ID="LiteralPrice" runat="server"></asp:Literal>
-            <asp:CheckBox ID="CheckBoxInStock" runat="server" />
-        </asp:PlaceHolder>
-    </div>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">    
+
+<div class="container">    
+  <div class="row">
+      <asp:Literal ID="dynamicArticles" runat="server"></asp:Literal>
+  </div>
+</div><br>
 </asp:Content>
--- Projekt1 KAB/index.aspx.cs
+++ Projekt1 KAB/index.aspx.cs
@@ -17,17 +17,27 @@ namespace Projekt1_KAB
 
             List<Article> articles = SQL.ReadArticles();
 
+            string html = "";
+
             foreach (Article item in articles)
             {
-                LiteralArticleName.Text +=$"<ul><li>{item.ArticleName}</li>";
-                LiteralArticleDescription.Text += $"<li>{item.ArticleDescription}</li>";
-                LiteralPrice.Text += $"<li>{item.ArticlePrice}</li></ul>";
-
-                if (item.IsInStock == true)
-                {
-                    CheckBoxInStock.Checked = true;
-                }
+                html += $"<div class=\"col-sm-4\"><div class=\"panel panel-primary\">";
+                html += $"<div class=\"panel-heading\">{item.ArticleName}</div><div class=\"panel-body\">";
+                html += $"<img src=\"https://placehold.it/150x80?text=IMAGE\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\"></div>";
+                html += $"<div class=\"panel - footer\">{item.ArticlePrice}</div></div></div>";
             }
+            dynamicArticles.Text = html;
+
+            //Article article = mySQL.GetArticleByID(2001);
+
+            //Literal1.Text = article.ArticleName;
+            //Literal2.Text = article.ArticlePrice.ToString();
+
+            //if (article.IsInStock)
+            //{
+            //    CheckBoxInStock.Checked = true;
+            //}
+
 
         }
 
