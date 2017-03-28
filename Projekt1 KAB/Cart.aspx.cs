e32998cf524b6b4ee64a4125d472d9b2be2fcba2
SofiaUnnerstale <sofia.unnerstale@gmail.com>
2017-03-28 10:23:27 +0200

    Added cart

---
+++ Projekt1 KAB/Cart.aspx.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibraryKAB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projekt1_KAB
+{
+    public partial class Cart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Cart"] != null)
+            {
+                Literal1.Text = ((List<Article>)Session["Cart"]).Count.ToString();
+
+                List<Article> cart = (List<Article>)Session["Cart"];
+
+                foreach (var item in cart)
+                {
+                    Literal1.Text += $"{item.ArticleName} <br>";
+                }
+            }
+
+            
+        }
+    }
+}
