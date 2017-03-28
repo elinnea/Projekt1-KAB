f8707da8168fe080257bf1a2636719c357f762be
elinnea <elinnea.larsson@gmail.com>
2017-03-28 11:26:28 +0200

    work in progress på article

--- Projekt1 KAB/article.aspx.cs
+++ Projekt1 KAB/article.aspx.cs
@@ -10,30 +10,38 @@ namespace Projekt1_KAB
 {
     public partial class article : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            Article article = SQL.GetArticleByID(2001);
-            articleContent.Text = $"{article.ArticleName} {article.ArticlePrice}kr <br> {article.ArticleDescription}";
-            if (article.IsInStock)
-            {
-                CheckBoxInStock.Checked = true;                
-            }
+//        protected void Page_Load(object sender, EventArgs e)
+//        {
+//            Article article = SQL.GetArticleByID(2001);
+//            articleContent.Text = $"{article.ArticleName} {article.ArticlePrice}kr <br> {article.ArticleDescription}";
+//            if (article.IsInStock)
+//            {
+//                CheckBoxInStock.Checked = true;                
+//            }
+//            string html = "";
 
-//            string html ="< div class="container">    
-//	<div class="panel panel-primary">
-//    <div class="panel-heading">PRODUKTNAMN</div>
-//        	<div class="panel-body">
-//            	<div class="row">
-//        		<div class="col-sm-6"><img src = "https://placehold.it/150x120?text=IMAGE" class="img-responsive" alt="Image"></div> 
-//        		<div class="col-sm-6">en massa text,en massa text,en massa text,en massa text,en massa text,en massa text,en massa  text,en massa text,</div>
-//      			</div> 
-//                <div>
-//                	<input type = "button" value="Lägg i varukorgen" id="addToCart" onclick="AddToCart_Click">
+//            html += $"<div class=\"container\">< div class=\"panel panel-primary\">";
+//            html += $"<div class=\"panel - heading\">{article.ArticleName}</div>";
+//            html += $"<div class=\"panel-body\"><div class=\"row\">";
+//            html += $"<div class=\"col - sm - 6\" style=\"float:left\"><img src = \"{ article.ArticleImage}\" class=\"img - responsive\" alt=\"Image\"></div>";
+
+//            articleContent = html;
+
+            
+    
+        	
+                   
+//                  <div class="col-sm-6"style="float:right">{article.ArticleDescription}</div>
+
+//                </div> 
+//                <div style = "float:right" >
+//                	{article.ArticlePrice}
+//                    <input type = "button" value="Lägg i varukorgen" id="addToCart" onclick="AddToCart">
 //                </div>
 //            </div>       
 //    </div>
-//</div>"
+//</div>
 
-        }
-}
+//        }
+    }
 }
