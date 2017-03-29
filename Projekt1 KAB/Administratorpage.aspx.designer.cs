e7b0cf1e92a904dee7578738d6c4a9a356893e4b
Mats Åström <mats_astroem@yahoo.se>
2017-03-29 11:03:25 +0200

    Adminpage change.

--- Projekt1 KAB/Administratorpage.aspx.designer.cs
+++ Projekt1 KAB/Administratorpage.aspx.designer.cs
@@ -130,6 +130,15 @@ namespace Projekt1_KAB {
         protected global::System.Web.UI.WebControls.Button ButtonReadArticles;
         
         /// <summary>
+        /// ButtonReadInActiveArticles control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonReadInActiveArticles;
+        
+        /// <summary>
         /// ListBoxArticles control.
         /// </summary>
         /// <remarks>
