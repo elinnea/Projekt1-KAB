52716c602810276dece5a0094b94fd7c3a5afcee
elinnea <elinnea.larsson@gmail.com>
2017-03-29 11:10:36 +0200

    Betalsida för kortuppgifter, totalsumma saknas

---
+++ Projekt1 KAB/PayOrder.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projekt1_KAB
+{
+    public partial class PayOrder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            decimal totalPrice = (Convert.ToDecimal(Session["totalPrice"]));
+
+            //LiteralAmount.Text = totalPrice.ToString("C2", CultureInfo.CurrentCulture);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("OrderConfirmation.aspx");
+        }
+    }
+}
